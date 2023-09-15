eb549da479e48ef3684f08ebc5a32c24d6b9a242
sabrina <n.sabrina@aven-sys.com>
2023-09-15 15:03:52 +0800

    changes to header and contact bug fix

--- common/src/Layouts/LayoutMenuData.js
+++ common/src/Layouts/LayoutMenuData.js
@@ -6,16 +6,6 @@ const Navdata = () => {
   //state data
   const [isDashboard, setIsDashboard] = useState(false);
   const [isApps, setIsApps] = useState(false);
-  const [isAuth, setIsAuth] = useState(false);
-  const [isPages, setIsPages] = useState(false);
-  const [isBaseUi, setIsBaseUi] = useState(false);
-  const [isAdvanceUi, setIsAdvanceUi] = useState(false);
-  const [isForms, setIsForms] = useState(false);
-  const [isTables, setIsTables] = useState(false);
-  const [isCharts, setIsCharts] = useState(false);
-  const [isIcons, setIsIcons] = useState(false);
-  const [isMaps, setIsMaps] = useState(false);
-  const [isMultiLevel, setIsMultiLevel] = useState(false);
 
   const [isAccounts, setIsAccounts] = useState(false);
   const [isContacts, setIsContacts] = useState(false);
@@ -23,42 +13,6 @@ const Navdata = () => {
   const [isCandidates, setIsCandidates] = useState(false);
   const [isReports, setIsReports] = useState(false);
   const [isSettings, setIsSettings] = useState(false);
-  // Apps
-  const [isEmail, setEmail] = useState(false);
-  const [isSubEmail, setSubEmail] = useState(false);
-  const [isEcommerce, setIsEcommerce] = useState(false);
-  const [isProjects, setIsProjects] = useState(false);
-  const [isTasks, setIsTasks] = useState(false);
-  const [isCRM, setIsCRM] = useState(false);
-  const [isCrypto, setIsCrypto] = useState(false);
-  const [isInvoices, setIsInvoices] = useState(false);
-  const [isSupportTickets, setIsSupportTickets] = useState(false);
-  const [isNFTMarketplace, setIsNFTMarketplace] = useState(false);
-  const [isJobs, setIsJobs] = useState(false);
-  const [isJobList, setIsJobList] = useState(false);
-  const [isCandidateList, setIsCandidateList] = useState(false);
-
-  // Authentication
-  const [isSignIn, setIsSignIn] = useState(false);
-  const [isSignUp, setIsSignUp] = useState(false);
-  const [isPasswordReset, setIsPasswordReset] = useState(false);
-  const [isPasswordCreate, setIsPasswordCreate] = useState(false);
-  const [isLockScreen, setIsLockScreen] = useState(false);
-  const [isLogout, setIsLogout] = useState(false);
-  const [isSuccessMessage, setIsSuccessMessage] = useState(false);
-  const [isVerification, setIsVerification] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  // Pages
-  const [isProfile, setIsProfile] = useState(false);
-  const [isLanding, setIsLanding] = useState(false);
-
-  // Charts
-  const [isApex, setIsApex] = useState(false);
-
-  // Multi Level
-  const [isLevel1, setIsLevel1] = useState(false);
-  const [isLevel2, setIsLevel2] = useState(false);
 
   const [iscurrentState, setIscurrentState] = useState("Dashboard");
 
@@ -96,46 +50,6 @@ const Navdata = () => {
     if (iscurrentState !== "Settings") {
       setIsSettings(false);
     }
-    if (iscurrentState !== "Apps") {
-      setIsApps(false);
-    }
-    if (iscurrentState !== "Auth") {
-      setIsAuth(false);
-    }
-    if (iscurrentState !== "Pages") {
-      setIsPages(false);
-    }
-    if (iscurrentState !== "BaseUi") {
-      setIsBaseUi(false);
-    }
-    if (iscurrentState !== "AdvanceUi") {
-      setIsAdvanceUi(false);
-    }
-    if (iscurrentState !== "Forms") {
-      setIsForms(false);
-    }
-    if (iscurrentState !== "Tables") {
-      setIsTables(false);
-    }
-    if (iscurrentState !== "Charts") {
-      setIsCharts(false);
-    }
-    if (iscurrentState !== "Icons") {
-      setIsIcons(false);
-    }
-    if (iscurrentState !== "Maps") {
-      setIsMaps(false);
-    }
-    if (iscurrentState !== "MuliLevel") {
-      setIsMultiLevel(false);
-    }
-    if (iscurrentState === "Widgets") {
-      history("/widgets");
-      document.body.classList.add("twocolumn-panel");
-    }
-    if (iscurrentState !== "Landing") {
-      setIsLanding(false);
-    }
   }, [
     history,
     iscurrentState,
@@ -147,16 +61,6 @@ const Navdata = () => {
     isReports,
     isJob,
     isSettings,
-    isAuth,
-    isPages,
-    isBaseUi,
-    isAdvanceUi,
-    isForms,
-    isTables,
-    isCharts,
-    isIcons,
-    isMaps,
-    isMultiLevel,
   ]);
 
   const menuItems = [
@@ -217,25 +121,52 @@ const Navdata = () => {
       click: function (e) {
         e.preventDefault();
         setIsContacts(!isContacts);
-        setIscurrentState("Contact");
+        setIscurrentState("Contacts");
         updateIconSidebar(e);
       },
       stateVariables: isContacts,
       subItems: [
         {
-          id: "contacts",
+          id: "allContacts",
           label: "All Contacts",
           link: "/contacts",
           parentId: "contact",
         },
         {
           id: "newContact",
-          label: "Create New Contact",
+          label: "Create New Contacts",
           link: "/contact/contact-creation",
           parentId: "contact",
         },
       ],
     },
+    // {
+    //   id: "contact",
+    //   label: "Contacts",
+    //   icon: "ri-user-3-fill",
+    //   link: "/#",
+    //   click: function (e) {
+    //     e.preventDefault();
+    //     setIsContacts(!isContacts);
+    //     setIscurrentState("Contact");
+    //     updateIconSidebar(e);
+    //   },
+    //   stateVariables: isContacts,
+    //   subItems: [
+    //     {
+    //       id: "contacts",
+    //       label: "All Contacts",
+    //       link: "/contacts",
+    //       parentId: "contact",
+    //     },
+    //     {
+    //       id: "newContact",
+    //       label: "Create New Contact",
+    //       link: "/contact/contact-creation",
+    //       parentId: "contact",
+    //     },
+    //   ],
+    // },
 
     // Job Openings
     {
