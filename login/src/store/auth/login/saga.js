6f3ed16c4c17b4b9b98762208d3b84bdaea715d9
hxg-avensys <H.Xiang@aven-sys.com>
2023-11-02 09:18:28 +0800

    Add login and logout toast

--- login/src/store/auth/login/saga.js
+++ login/src/store/auth/login/saga.js
@@ -1,4 +1,5 @@
 import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { toast } from "react-toastify";
 
 // Login Redux States
 import { LOGIN_USER, LOGOUT_USER, SOCIAL_LOGIN } from "./actionTypes";
@@ -16,6 +17,7 @@ function* loginUser({ payload: { user, history } }) {
       yield put(loginSuccess(response));
       sessionStorage.setItem("authUser", JSON.stringify(response));
       history("/dashboard");
+      toast.success("Login Successfully");
     } else {
       yield put(apiError(response));
     }
@@ -34,6 +36,7 @@ function* logoutUser({ payload: { history } }) {
       sessionStorage.removeItem("authUser");
       yield put(deleteProfile())
       yield put(logoutUserSuccess(LOGOUT_USER, true));
+      toast.success("Logout Successfully");
     }
   } catch (error) {
     yield put(apiError(LOGOUT_USER, error));
