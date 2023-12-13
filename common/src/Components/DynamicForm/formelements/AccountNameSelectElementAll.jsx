dc2c7fa7e0595537e6e2d8521f5f90e811f55508
hxg-avensys <H.Xiang@aven-sys.com>
2023-12-13 13:47:29 +0800

    Changed account name and contact select

--- common/src/Components/DynamicForm/formelements/AccountNameSelectElementAll.jsx
+++ common/src/Components/DynamicForm/formelements/AccountNameSelectElementAll.jsx
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Select from "react-select";
 import {
   fetchAccountContacts,
-  fetchAccountNamesAll
+  fetchAccountNamesAll,
 } from "../../../store/actions";
 
-const AccountNameSelectElementAll = ({ formik, field, formStateHook }) => {
+const AccountNameSelectElementAll = ({
+  formik,
+  field,
+  formStateHook,
+  ...props
+}) => {
   const { formState } = formStateHook;
   const accountNamesData = useSelector(
     (state) => state.AccountNamesReducer.accountNames
   );
   const dispatch = useDispatch();
   const [fetchData, setFetchData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [options, setOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   useEffect(() => {
     dispatch(fetchAccountNamesAll());
@@ -34,39 +43,112 @@ const AccountNameSelectElementAll = ({ formik, field, formStateHook }) => {
         fetchAccountContacts(parseInt(namesToid(formik?.values?.[field.name])))
       );
     }
+    if (formik?.values?.[field.name] === "") {
+      dispatch(fetchAccountContacts(-99));
+    }
   }, [formik?.values?.[field.name], accountNamesData]);
 
-  // Handle Account Change, get account contact details
-  const handleAccountChange = (e) => {
-    if (e.target.value) {
-      formik.handleChange(e);
-      dispatch(fetchAccountContacts(parseInt(namesToid(e.target.value))));
+  // Helper Functions
+  function mapToOptionFormat(apiData) {
+    return apiData.map((item) => ({
+      label: item?.name,
+      value: item?.name,
+    }));
+  }
+
+  function getSingleExistingDataOptions(data) {
+    if (!data) return undefined;
+    return options?.find((option) => option.label === data);
+  }
+
+  // Get Data for normal API calls without parents
+  useEffect(() => {
+    if (accountNamesData) {
+      setOptions(mapToOptionFormat(accountNamesData));
+    }
+  }, [accountNamesData]);
+
+  const handleInputChange = (inputValue) => {
+    setSearch(inputValue);
+  };
+
+  const handleChange = (selectedOptions) => {
+    setSelectedOptions(selectedOptions);
+    if (selectedOptions == null) {
+      formik?.setFieldValue(field.name, "");
+    } else {
+      formik?.setFieldValue(field.name, selectedOptions.label);
     }
   };
 
+  useEffect(() => {
+    setSelectedOptions(null);
+    if (
+      formik?.values?.[field.name] &&
+      formik?.values?.[field.name] !== "" &&
+      options
+    ) {
+      const setOption = getSingleExistingDataOptions(
+        formik?.values?.[field.name]
+      );
+      if (setOption) {
+        setSelectedOptions(setOption);
+      } else {
+        setSelectedOptions(null);
+      }
+    } else {
+      setSelectedOptions(null);
+    }
+  }, [formik?.values?.[field.name], options]);
+
+  const isValid = !props?.error;
+
+  const customStyles = {
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 9999,
+    }),
+    control: (base, state) => ({
+      ...base,
+      // state.isFocused can display different borderColor if you need it
+      borderColor: state.isFocused ? "#8AAED6" : isValid ? "#8AAED6" : "red",
+      // overwrittes hover style
+      "&:hover": {
+        borderColor: state.isFocused ? "#8AAED6" : isValid ? "#8AAED6" : "red",
+      },
+      backgroundColor: state.isDisabled ? "#EFF2F7" : base.backgroundColor,
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color: state.isDisabled ? "black !important" : provided.color,
+    }),
+  };
+
+  const noOptionsMessage = () => null; // Return null to prevent the message from showing
+
   return (
     <div>
-      {fetchData && (
-        <select
-          name={field.name}
-          onChange={handleAccountChange}
-          onBlur={formik.handleBlur}
-          className={`form-select ${
-            formik?.values?.[field.name] === "" ||
-            formik?.values?.[field.name] === undefined
-              ? "text-muted"
-              : ""
-          }`}
-          value={formik?.values?.[field.name]}
-          disabled={formState === "view" ? true : false}
-        >
-          <option value="">{field.placeholder}</option>
-          {fetchData?.map((item, index) => (
-            <option key={index} value={item?.name}>
-              {item?.name}
-            </option>
-          ))}
-        </select>
+      {props?.label && (
+        <Label className="form-label" htmlFor={props?.htmlFor ?? "input-field"}>
+          {props?.label}
+        </Label>
+      )}
+      <Select
+        styles={customStyles}
+        value={selectedOptions}
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        inputValue={search}
+        menuShouldScrollIntoView={false}
+        isClearable
+        isSearchable
+        placeholder={field.placeholder ?? "Search..."}
+        options={options}
+        noOptionsMessage={noOptionsMessage}
+        isDisabled={formState === "view" ? true : false}
+      />
+      {props?.error && (
+        <FormFeedback type="invalid">{props?.error}</FormFeedback>
       )}
     </div>
   );
