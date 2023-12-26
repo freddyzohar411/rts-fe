5846c5cdee6488ca0b4432a1722687fcb2971b54
hxg-avensys <H.Xiang@aven-sys.com>
2023-12-26 18:16:43 +0800

    Updated and fix

--- common/src/Components/DynamicForm/components/formbuilder/DroppableList.jsx
+++ common/src/Components/DynamicForm/components/formbuilder/DroppableList.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../formelements/formElements_helper";
 import Setting from "./Setting";
 import DnDWrapper from "../dragndrop/DnDWrapper";
+import FieldToolTip from "../formdisplay/FieldToolTip";
 
 const DroppableList = ({
   row,
@@ -75,6 +76,7 @@ const DroppableList = ({
                   const field = formFields.find(
                     (field) => field.fieldId === fieldId
                   );
+                  console.log("Field Information", field.information);
                   if (
                     !showAll
                       ? checkVisibleConditions(field, formik) &&
@@ -100,12 +102,19 @@ const DroppableList = ({
                           >
                             <div className="mb-4">
                               <div className="d-flex justify-content-between align-items-center mb-2 ">
-                                <label
-                                  htmlFor={field.name}
-                                  className="form-label"
-                                >
-                                  {field.label}
-                                </label>
+                                <div className="d-flex gap-2">
+                                  <label
+                                    htmlFor={field.name}
+                                    className="form-label"
+                                  >
+                                    {field.label}
+                                  </label>
+                                  {(field.information === "true" ||
+                                    field.information === true) && (
+                                    <FieldToolTip field={field} />
+                                  )}
+                                </div>
+
                                 <div className="editable">
                                   <div className={`d-flex gap-2`}>
                                     {(field?.fieldType === "custom" ||
@@ -202,7 +211,7 @@ const DroppableList = ({
             type="text"
             value={title}
             className="mb-3 fw-bold h6 form-control"
-            style={{borderColor: "lightgrey"}}
+            style={{ borderColor: "lightgrey" }}
             onChange={(e) => setRowtitle(row, e.target.value)}
           />
         )}
