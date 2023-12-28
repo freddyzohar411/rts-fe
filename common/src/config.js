b94ed386e613ba952b3784db66adc3c1254f49d0
rku-avensys <rahul@aven-sys.com>
2023-12-28 13:56:30 +0530

    env file changes

--- common/src/config.js
+++ common/src/config.js
@@ -1,57 +1,62 @@
-export const API_URL = "http://localhost:8090";
-export const PUBLIC_URL = "http://localhost:3001";
-export const LOGIN_MS_URL = "http://localhost:3000";
+export const SERVER_IP = process.env.REACT_APP_API_BASE_URL;
+
+export const API_URL = `${SERVER_IP}:8090`;
+export const PUBLIC_URL = `${SERVER_IP}:3001`;
+export const LOGIN_MS_URL = `${SERVER_IP}:3000`;
 
 // Account Service
-export const ACCOUNT_URL = "http://localhost:8100";
+export const ACCOUNT_URL = `${SERVER_IP}:8100`;
 
 // Indusrty Service
-export const INDUSTRY_URL = "http://localhost:8200";
+export const INDUSTRY_URL = `${SERVER_IP}:8200`;
 
 // Address Service
-export const ADDRESS_URL = "http://localhost:8300";
+export const ADDRESS_URL = `${SERVER_IP}:8300`;
 
 // Document Service
-export const DOCUMENT_URL = "http://localhost:8500";
+export const DOCUMENT_URL = `${SERVER_IP}:8500`;
 
 // Geo, Currency Service
-export const GEO_URL = "http://localhost:8600";
+export const GEO_URL = `${SERVER_IP}:8600`;
 
 // Contact Service
-export const CONTACT_URL = "http://localhost:8700";
+export const CONTACT_URL = `${SERVER_IP}:8700`;
 
 // Account Instruction Service
-export const ACCOUNT_INSTRUCTION_URL = "http://localhost:8800";
+export const ACCOUNT_INSTRUCTION_URL = `${SERVER_IP}:8800`;
 
 // Department Service
-export const DEPARTMENT_URL = "http://localhost:8900";
+export const DEPARTMENT_URL = `${SERVER_IP}:8900`;
 
 // Role Service
-export const ACCESS_URL = "http://localhost:8091";
+export const ACCESS_URL = `${SERVER_IP}:8091`;
 
 // Group Service
-export const GROUP_URL = "http://localhost:8092";
+export const GROUP_URL = `${SERVER_IP}:8092`;
 
 // Candidate Service
-export const CANDIDATE_URL = "http://localhost:8050";
+export const CANDIDATE_URL = `${SERVER_IP}:8050`;
 
 // Job Service
-export const JOB_URL = "http://localhost:9200";
+export const JOB_URL = `${SERVER_IP}:9200`;
 
 // Form Service
-export const FORM_URL = "http://localhost:9400";
+export const FORM_URL = `${SERVER_IP}:9400`;
 
 // Candidate/Work experience service
-export const CANDIDATE_WORK_EXPERIENCE_URL = "http://localhost:9500";
+export const CANDIDATE_WORK_EXPERIENCE_URL = `${SERVER_IP}:9500`;
 
 // Candidate/Education service
-export const CANDIDATE_EDUCATION_DETAILS_URL = "http://localhost:9600";
+export const CANDIDATE_EDUCATION_DETAILS_URL = `${SERVER_IP}:9600`;
 
 // Candidate/Certificate service
-export const CANDIDATE_CERTIFICATE_URL = "http://localhost:9700";
+export const CANDIDATE_CERTIFICATE_URL = `${SERVER_IP}:9700`;
 
 // Candidate/Language service
-export const CANDIDATE_LANGUAGES_URL = "http://localhost:9800";
+export const CANDIDATE_LANGUAGES_URL = `${SERVER_IP}:9800`;
 
 // Candidate/Employer details service
-export const CANDIDATE_EMPLOYER_DETAILS_URL = "http://localhost:9900";
+export const CANDIDATE_EMPLOYER_DETAILS_URL = `${SERVER_IP}:9900`;
+
+// Job Service
+export const DASHBOARD_URL = `${SERVER_IP}:9300`;
--- dashboard/src/helpers/url_helper.js
+++ dashboard/src/helpers/url_helper.js
@@ -5,8 +5,5 @@ export const GET_MONTHREVENUE_DATA = "/monthRevenue-data";
 export const GET_HALFYEARREVENUE_DATA = "/halfYearRevenue-data";
 export const GET_YEARREVENUE_DATA = "/yearRevenue-data";
 
-// Job Service
-export const DASHBOARD_URL = "http://localhost:9300";
-
 //Job
 export const BASE_DASHBOARD = "/api/dashboard";
