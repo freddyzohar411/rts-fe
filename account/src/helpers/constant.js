7119a5427521ff94c9023a52e1097dd7a6978a3f
ppa-avensys <142867384+ppa-avensys@users.noreply.github.com>
2024-01-04 08:56:59 +0530

    Update constant.js

--- account/src/helpers/constant.js
+++ account/src/helpers/constant.js
@@ -2,7 +2,7 @@ import * as PortURL from "@workspace/common/src/config";
 import * as BaseURL from "./url_helper";
 
 export const accountModuleURL = {
-  account_account: `${PortURL.ACCOUNT_URL}${BaseURL.BASE_ACCOUNTS}`,
+  account_account: `${PortURL.ACCOUNT_URL}${BaseURL.BASE_ACCOUNTS}/add`,
   account_contact: `${PortURL.CONTACT_URL}${BaseURL.BASE_CONTACTS}`,
   account_document: `${PortURL.DOCUMENT_URL}${BaseURL.BASE_DOCUMENTS}`,
   account_instruction: `${PortURL.ACCOUNT_INSTRUCTION_URL}${BaseURL.BASE_CLIENT_INSTRUCTIONS}`,
