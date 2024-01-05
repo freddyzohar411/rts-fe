b0e5edfb805f491f299c85f9295695d1d125522d
hxg-avensys <H.Xiang@aven-sys.com>
2024-01-05 11:12:27 +0800

    Merge

--- template/src/components/TemplateBuilder/EditorElement2.jsx
+++ template/src/components/TemplateBuilder/EditorElement2.jsx
@@ -26,6 +26,10 @@ const EditorElement2 = ({
     }
   }, [injectVariable]);
 
+  const handleFileUpload = (file) => {
+    console.log("File", file);
+  };
+
   return (
     <>
       <Editor
@@ -61,9 +65,41 @@ const EditorElement2 = ({
             "bold italic forecolor | alignleft aligncenter " +
             "alignright alignjustify | bullist numlist outdent indent | hr |" +
             "removeformat | " +
-            "table | emoticons | code | charmap | fullscreen | preview | help",
+            "table | emoticons | code | charmap | image | media | fullscreen | preview | help",
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          // file_picker_callback: (cb, value, meta) => {
+          //   const input = document.createElement("input");
+          //   input.setAttribute("type", "file");
+          //   input.setAttribute("accept", "image/*");
+
+          //   input.addEventListener("change", (e) => {
+          //     const file = e.target.files[0];
+          //     console.log("File In Event", file)
+
+          //     const reader = new FileReader();
+          //     reader.addEventListener("load", () => {
+          //       /*
+          //           Note: Now we need to register the blob in TinyMCEs image blob
+          //           registry. In the next release this part hopefully won't be
+          //           necessary, as we are looking to handle it internally.
+          //         */
+          //       const id = "blobid" + new Date().getTime();
+          //       const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+          //       const base64 = reader.result.split(",")[1];
+          //       const blobInfo = blobCache.create(id, file, base64);
+          //       blobCache.add(blobInfo);
+
+          //       /* call the callback and populate the Title field with the file name */
+          //       // cb(blobInfo.blobUri(), { title: file.name });
+          //       handleFileUpload(file);
+          //     });
+          //     reader.readAsDataURL(file);
+          //   });
+
+          //   input.click();
+          // },
+          // file_picker_types: "image",
         }}
         onEditorChange={(value) => {
           console.log("Value", value);
