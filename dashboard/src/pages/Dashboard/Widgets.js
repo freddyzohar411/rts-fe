6659d2dcd6cf30e268982aa0d0a451352c6501ae
sabrina <142870152+sbn-avensys@users.noreply.github.com>
2024-01-09 10:14:50 +0800

    Dashboard Changes

--- dashboard/src/pages/Dashboard/Widgets.js
+++ dashboard/src/pages/Dashboard/Widgets.js
@@ -1,16 +1,12 @@
 import React from "react";
 import CountUp from "react-countup";
-import { Badge, Card, CardBody, Col } from "reactstrap";
+import { Row, Badge, Card, CardBody, Col, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import { recruiterDashboard, salesDashboard } from "@workspace/common";
 import { useUserAuth } from "@workspace/login";
-import { useSelector } from "react-redux";
 
 const Widgets = () => {
   const { Permission, checkAllPermission } = useUserAuth();
-
-  const jobCounts = useSelector((state) => state.DashboardEcommerce.jobCounts);
-
   const renderStatusArray = (statusArray) => {
     if (!statusArray || !Array.isArray(statusArray)) {
       return null;
@@ -21,10 +17,17 @@ const Widgets = () => {
         {statusArray.map(({ status, count }, index) => (
           <div
             key={index}
-            className="d-flex flex-row justify-content-between mb-2 gap-5 fs-5"
+            className="d-flex flex-row justify-content-between mt-0 mb-1 gap-3 fs-5"
           >
-            <span className="fw-semibold">{status}</span>
-            <Badge color="dark">{count}</Badge>
+            <span className="fw-medium">{status}</span>
+            <Badge
+              className="bg-white text-dark border border-1 border-dark d-flex justify-content-center align-items-center"
+              style={{
+                width: "40px",
+              }}
+            >
+              {count}
+            </Badge>
           </div>
         ))}
       </div>
@@ -32,105 +35,170 @@ const Widgets = () => {
   };
   return (
     <React.Fragment>
-      {checkAllPermission([Permission.ACCOUNT_WRITE]) &&
-        salesDashboard.map((item, key) => (
-          <Col xl={3} md={6} key={key}>
-            <Link to={item.link}>
-              <Card className="card-animate">
-                <CardBody>
-                  <div className="d-flex align-items-center">
-                    <div className="flex-grow-1 overflow-hidden">
-                      <p className="text-uppercase fw-medium text-muted text-truncate mb-0">
-                        {item.label}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="d-flex align-items-end justify-content-between mt-4">
-                    <div>
-                      <h4 className="fs-22 fw-semibold ff-secondary mb-4">
-                        <span className="counter-value" data-target="559.25">
-                          <CountUp
-                            start={0}
-                            prefix={item.prefix}
-                            suffix={item.suffix}
-                            separator={item.separator}
-                            end={jobCounts?.[item?.key] ?? 0}
-                            decimals={item.decimals}
-                            duration={4}
-                          />
-                          {item.statusArray &&
-                            renderStatusArray(item.statusArray)}
-                          {item.data && item.data}
+      <Row className="ms-1">
+        {checkAllPermission([Permission.ACCOUNT_WRITE]) &&
+          salesDashboard.map((item, key) => (
+            <Col
+              xl={3}
+              md={6}
+              key={key}
+              className="mb-4"
+              style={{ height: "220px" }}
+            >
+              <Link to={item.link}>
+                <Card className="card-animate d-flex flex-column h-100">
+                  <CardBody className="p-4 d-flex flex-column justify-content-between">
+                    <div className="d-flex flex-row justify-content-between align-items-start">
+                      <div className="flex-grow-1 overflow-hidden">
+                        <p
+                          className="text-uppercase fs-4 fw-semibold text-truncate mb-0"
+                          style={{ color: "#000000" }}
+                        >
+                          {item.label}
+                        </p>
+                        {item.subLabel ? (
+                          <p
+                            className="text-uppercase fs-5 fw-semibold text-truncate mb-0"
+                            style={{ color: "#83A9E2" }}
+                          >
+                            {item.subLabel}
+                          </p>
+                        ) : null}
+                      </div>
+                      <div className="avatar-md flex-shrink-0">
+                        <span
+                          className={
+                            "avatar-title rounded-circle fs-1 bg-" +
+                            item.bgcolor +
+                            "-subtle"
+                          }
+                        >
+                          <i
+                            className={`text-${item.bgcolor} ${item.icon}`}
+                          ></i>
                         </span>
-                      </h4>
+                      </div>
                     </div>
-                    <div className="avatar-sm flex-shrink-0">
-                      <span
-                        className={
-                          "avatar-title rounded fs-3 bg-" +
-                          item.bgcolor +
-                          "-subtle"
-                        }
-                      >
-                        <i className={`text-${item.bgcolor} ${item.icon}`}></i>
-                      </span>
-                    </div>
-                  </div>
-                </CardBody>
-              </Card>
-            </Link>
-          </Col>
-        ))}
 
-      {checkAllPermission([Permission.CANDIDATE_WRITE]) &&
-        recruiterDashboard.map((item, key) => (
-          <Col xl={3} md={6} key={key}>
-            <Link to={item.link}>
-              <Card className="card-animate">
-                <CardBody>
-                  <div className="d-flex align-items-center">
-                    <div className="flex-grow-1 overflow-hidden">
-                      <p className="text-uppercase fw-medium text-muted text-truncate mb-0">
-                        {item.label}
-                      </p>
+                    <div className="d-flex align-items-end justify-content-between">
+                      {item.statusArray && renderStatusArray(item.statusArray)}
+                      <div>
+                        <h4 className="fs-1 fw-bold ff-secondary">
+                          <span className="counter-value" data-target="559.25">
+                            {item.counter > 0 && (
+                              <CountUp
+                                start={0}
+                                prefix={item.prefix}
+                                suffix={item.suffix}
+                                separator={item.separator}
+                                end={item.counter}
+                                decimals={item.decimals}
+                                duration={4}
+                              />
+                            )}
+                          </span>
+                          <span>{item.data}</span>
+                        </h4>
+                      </div>
+                      <div>
+                        <a
+                          href=""
+                          className="fs-5 fw-semibold"
+                          style={{ color: "#18AB00" }}
+                        >
+                          View All
+                        </a>
+                      </div>
+                      {item.footer ? <p>{item.footer}</p> : ""}
                     </div>
-                  </div>
-                  <div className="d-flex align-items-end justify-content-between mt-4">
-                    <div>
-                      <h4 className="fs-22 fw-semibold ff-secondary mb-4">
-                        <span className="counter-value" data-target="559.25">
-                          <CountUp
-                            start={0}
-                            prefix={item.prefix}
-                            suffix={item.suffix}
-                            separator={item.separator}
-                            end={jobCounts?.[item?.key] ?? 0}
-                            decimals={item.decimals}
-                            duration={4}
-                          />
-                          {item.statusArray &&
-                            renderStatusArray(item.statusArray)}
-                          {item.data && item.data}
+                  </CardBody>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+      </Row>
+      <Row className="ms-1">
+        {checkAllPermission([Permission.CANDIDATE_WRITE]) &&
+          recruiterDashboard.map((item, key) => (
+            <Col
+              xl={3}
+              md={6}
+              key={key}
+              className="mb-4"
+              style={{ height: "220px" }}
+            >
+              <Link to={item.link}>
+                <Card className="card-animate d-flex flex-column h-100">
+                  <CardBody className="p-4 d-flex flex-column justify-content-between">
+                    <div className="d-flex flex-row justify-content-between align-items-start">
+                      <div className="flex-grow-1 overflow-hidden">
+                        <p
+                          className="text-uppercase fs-4 fw-semibold text-truncate mb-0"
+                          style={{ color: "#000000" }}
+                        >
+                          {item.label}
+                        </p>
+                        {item.subLabel ? (
+                          <p
+                            className="text-uppercase fs-5 fw-semibold text-truncate mb-0"
+                            style={{ color: "#83A9E2" }}
+                          >
+                            {item.subLabel}
+                          </p>
+                        ) : null}
+                      </div>
+                      <div className="avatar-md flex-shrink-0">
+                        <span
+                          className={
+                            "avatar-title rounded-circle fs-1 bg-" +
+                            item.bgcolor +
+                            "-subtle"
+                          }
+                        >
+                          <i
+                            className={`text-${item.bgcolor} ${item.icon}`}
+                          ></i>
                         </span>
-                      </h4>
+                      </div>
                     </div>
-                    <div className="avatar-sm flex-shrink-0">
-                      <span
-                        className={
-                          "avatar-title rounded fs-3 bg-" +
-                          item.bgcolor +
-                          "-subtle"
-                        }
-                      >
-                        <i className={`text-${item.bgcolor} ${item.icon}`}></i>
-                      </span>
+
+                    <div className="d-flex align-items-end justify-content-between">
+                      {item.statusArray && renderStatusArray(item.statusArray)}
+                      <div>
+                        <h4 className="fs-1 fw-bold ff-secondary">
+                          <span className="counter-value" data-target="559.25">
+                            {item.counter > 0 && (
+                              <CountUp
+                                start={0}
+                                prefix={item.prefix}
+                                suffix={item.suffix}
+                                separator={item.separator}
+                                end={item.counter}
+                                decimals={item.decimals}
+                                duration={4}
+                              />
+                            )}
+                          </span>
+                          <span>{item.data}</span>
+                        </h4>
+                      </div>
+                      <div>
+                        <a
+                          href=""
+                          className="fs-5 fw-semibold"
+                          style={{ color: "#18AB00" }}
+                        >
+                          View All
+                        </a>
+                      </div>
+                      {item.footer ? <p>{item.footer}</p> : ""}
                     </div>
-                  </div>
-                </CardBody>
-              </Card>
-            </Link>
-          </Col>
-        ))}
+                  </CardBody>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+      </Row>
     </React.Fragment>
   );
 };
