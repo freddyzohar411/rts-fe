0a5ea94f8c8d70416f72d789c140433db9b0255e
sabrina <142870152+sbn-avensys@users.noreply.github.com>
2024-01-11 16:47:14 +0800

    FOD Tag Table Changes

--- job/src/components/CandidateDynamicTableWrapper/DynamicTableWrapper.jsx
+++ job/src/components/CandidateDynamicTableWrapper/DynamicTableWrapper.jsx
@@ -20,184 +20,53 @@ const DynamicTableWrapper = ({
   setCustomConfigData,
   confirmDelete,
 }) => {
-  const { Permission, checkAllPermission } = useUserAuth();
-  const [customViewShow, setCustomViewShow] = useState(false);
-  const [selectedOptGroup, setSelectedOptGroup] = useState(
-    CANDIDATE_INITIAL_OPTIONS
-  );
-
-  const [isCustomViewModalOpen, setIsCustomModalView] = useState(false);
-  const [deleteId, setDeleteId] = useState(null);
-  const [isRefresh, setIsRefresh] = useState(false);
-
-  const handleChange = (selected) => {
-    const selectedObjects = selected.map((value) => {
-      return optGroup.find((option) => option.value === value);
-    });
-    setSelectedOptGroup(selectedObjects);
-  };
-
   return (
     <React.Fragment>
-      <div className="page-content">
-        <GeneralModal
-          isOpen={isCustomViewModalOpen}
-          setIsOpen={setIsCustomModalView}
-        >
-          <div>
-            <Row>
-              <Col lg={12}>
-                <div className="mt-4 mt-lg-0 p-4">
-                  <h5 className="fs-14 mb-1">Account Fields Options</h5>
-                  <p className="text-muted">
-                    Select fields to show on account listing table
-                  </p>
-                  <DualListBox
-                    canFilter
-                    filterCallback={(optGroup, filterInput) => {
-                      if (filterInput === "") {
-                        return true;
-                      }
-                      return new RegExp(filterInput, "i").test(optGroup.label);
-                    }}
-                    filterPlaceholder="Search..."
-                    options={optGroup ?? []}
-                    selected={
-                      selectedOptGroup.map((option) => option?.value) ?? []
-                    }
-                    onChange={handleChange}
-                    icons={{
-                      moveLeft: (
-                        <span className="mdi mdi-chevron-left" key="key" />
-                      ),
-                      moveAllLeft: [
-                        <span
-                          className="mdi mdi-chevron-double-left"
-                          key="key"
-                        />,
-                      ],
-                      moveRight: (
-                        <span className="mdi mdi-chevron-right" key="key" />
-                      ),
-                      moveAllRight: [
-                        <span
-                          className="mdi mdi-chevron-double-right"
-                          key="key"
-                        />,
-                      ],
-                      moveDown: (
-                        <span className="mdi mdi-chevron-down" key="key" />
-                      ),
-                      moveUp: <span className="mdi mdi-chevron-up" key="key" />,
-                      moveTop: (
-                        <span className="mdi mdi-chevron-double-up" key="key" />
-                      ),
-                      moveBottom: (
-                        <span
-                          className="mdi mdi-chevron-double-down"
-                          key="key"
-                        />
-                      ),
-                    }}
-                  />
-                  <div className="d-flex justify-content-end">
-                    <button
-                      className="btn btn-custom-primary mt-3 "
-                      onClick={() => {
-                        setCustomConfigData(selectedOptGroup);
-                        setIsCustomModalView(false);
-                      }}
-                    >
-                      Set
-                    </button>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </GeneralModal>
-        <Container fluid>
-          <Row>
-            <Col lg={12}>
-              <Card className="m-3">
-                <CardBody>
-                  <div className="listjs-table">
-                    <Row className="d-flex column-gap-1 mb-3">
-                      {setSearch && (
-                        <Col>
-                          <div className="search-box">
-                            <form onSubmit={pageRequestSet.setSearchTerm}>
-                              <Input
-                                type="text"
-                                placeholder="Search"
-                                className="form-control search bg-light border-light"
-                                value={search}
-                                style={{ width: "350px" }}
-                                onChange={(e) => setSearch(e.target.value)}
-                              />
-                            </form>
+      <div>
+        <Row>
+          <Col lg={12}>
+            <div className="listjs-table">
+              <Row className="d-flex column-gap-1 mb-3">
+                <Col>
+                  <div className="d-flex flex-row justify-content-between align-items-baseline">
+                    <div className="text-decoration-underline">
+                      <span>Suitable Candidates </span>
+                      <span className="fw-semibold">(20)</span>
+                    </div>
+                    <div className="d-flex flex-row gap-3 align-items-baseline">
+                      <div className="search-box">
+                        {setSearch && (
+                          <form onSubmit={pageRequestSet.setSearchTerm}>
+                            <Input
+                              type="text"
+                              placeholder="Search"
+                              className="form-control search bg-light border-light"
+                              value={search}
+                              style={{ width: "350px" }}
+                              onChange={(e) => setSearch(e.target.value)}
+                            />
                             <i className="ri-search-line search-icon"></i>
-                          </div>
-                        </Col>
-                      )}
-                      <Col>
-                        <div className="d-flex column-gap-2 justify-content-end">
-                          <Button
-                            type="button"
-                            className="btn btn-custom-primary d-flex align-items-center column-gap-2"
-                          >
-                            <span>
-                              <i className="mdi mdi-download"></i>
-                            </span>
-                            Imports
-                          </Button>
-                          <Button
-                            type="button"
-                            onClick={() => {
-                              setIsCustomModalView(true);
-                              setCustomViewShow(!customViewShow);
-                            }}
-                            className="btn btn-custom-primary d-flex align-items-center column-gap-2"
-                          >
-                            <span>
-                              <i className="ri-settings-3-fill"></i>
-                            </span>
-                            Custom View
-                          </Button>
-                          {checkAllPermission([Permission.CANDIDATE_WRITE]) && (
-                            <Button
-                              type="button"
-                              className="btn btn-custom-primary"
-                            >
-                              <Link
-                                to="/candidates/create"
-                                style={{ color: "white" }}
-                              >
-                                Create New Candidate
-                              </Link>
-                            </Button>
-                          )}
-                          <Button
-                            type="button"
-                            className="btn btn-custom-primary"
-                          >
-                            <i className="ri-filter-line"></i>
-                          </Button>
-                        </div>
-                      </Col>
-                    </Row>
-                    <DynamicTable
-                      config={config}
-                      data={data}
-                      pageRequestSet={pageRequestSet}
-                      pageInfo={pageInfo}
-                    />
+                          </form>
+                        )}
+                      </div>
+                      <div>
+                        <Button className="btn btn-custom-primary">
+                          Tag Selected Candidates
+                        </Button>
+                      </div>
+                    </div>
                   </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+                </Col>
+              </Row>
+              <DynamicTable
+                config={config}
+                data={data}
+                pageRequestSet={pageRequestSet}
+                pageInfo={pageInfo}
+              />
+            </div>
+          </Col>
+        </Row>
       </div>
     </React.Fragment>
   );
---
+++ job/src/components/FODTagTable/FODCandidateListingConstants.js
@@ -0,0 +1,47 @@
+const CANDIDATE_INITIAL_OPTIONS = [
+    {
+      label: "Candidate First Name",
+      value: "candidateSubmissionData.firstName",
+      sort: true,
+      sortValue: "candidate_submission_data.firstName",
+    },
+    {
+      label: "Candidate Last Name",
+      value: "candidateSubmissionData.lastName",
+      sort: true,
+      sortValue: "candidate_submission_data.lastName",
+    },
+    {
+      label: "Candidate Nationality",
+      value: "candidateSubmissionData.candidateNationality",
+      sort: true,
+      sortValue: "candidate_submission_data.candidateNationality",
+    },
+    {
+      label: "Candidate Status",
+      value: "candidateSubmissionData.candidateStatus",
+      sort: true,
+      sortValue: "candidate_submission_data.candidateStatus",
+    },
+    {
+      label: "Visa Status",
+      value: "candidateSubmissionData.visaStatus",
+      sort: true,
+      sortValue: "candidate_submission_data.visaStatus",
+    },
+    {
+      label: "Phone",
+      value: "candidateSubmissionData.phone",
+      sort: true,
+      sortValue: "candidateSubmissionData.phone",
+    },
+    {
+      label: "Email",
+      value: "candidateSubmissionData.email",
+      sort: true,
+      sortValue: "candidate_submission_data.email",
+    },
+  ];
+  
+  export { CANDIDATE_INITIAL_OPTIONS };
+  
