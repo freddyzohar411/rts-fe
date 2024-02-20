fc0043b483d89e98759d003d31a4b99254537c90
rku-avensys <rahul@aven-sys.com>
2024-02-20 16:44:34 +0530

    removed unnecessary code

--- common/src/Components/DynamicForm/components/formbuilder/FieldBuilder.jsx
+++ common/src/Components/DynamicForm/components/formbuilder/FieldBuilder.jsx
@@ -429,20 +429,6 @@ const FieldBuilder = ({
         { label: "Yes", value: "true" },
         { label: "No", value: "false" },
       ],
-      // events: {
-      // onChange: (e) => {
-      //   formik.setValues({ ...formik.values, required: e.target.value });
-      //   if (e.target.value === "false") {
-      //     document.getElementsByName(
-      //       "requiredErrorMessage"
-      //     )[0].disabled = true;
-      //   } else {
-      //     document.getElementsByName(
-      //       "requiredErrorMessage"
-      //     )[0].disabled = false;
-      //   }
-      // },
-      // },
       apply: [
         "text",
         "email",
@@ -591,49 +577,17 @@ const FieldBuilder = ({
       type: "number",
       name: "maxValue",
       apply: ["number"],
-      // events: {
-      //   onChange: (e) => {
-      //     formik.setValues({ ...formik.values, maxValue: e.target.value });
-      //     if (e.target.value === "") {
-      //       document.getElementsByName(
-      //         "maxValueErrorMessage"
-      //       )[0].disabled = true;
-      //     } else {
-      //       document.getElementsByName(
-      //         "maxValueErrorMessage"
-      //       )[0].disabled = false;
-      //     }
-      //   },
-      // },
-      apply: ["number"],
     },
     {
       label: "Max Value Error Message",
       type: "text",
       name: "maxValueErrorMessage",
-      // events: {
-      //   disabled: true,
-      // },
       apply: ["number"],
     },
     {
       label: "Min Value",
       type: "number",
       name: "minValue",
-      // events: {
-      //   onChange: (e) => {
-      //     formik.setValues({ ...formik.values, minValue: e.target.value });
-      //     if (e.target.value === "") {
-      //       document.getElementsByName(
-      //         "minValueErrorMessage"
-      //       )[0].disabled = true;
-      //     } else {
-      //       document.getElementsByName(
-      //         "minValueErrorMessage"
-      //       )[0].disabled = false;
-      //     }
-      //   },
-      // },
       apply: ["number"],
     },
     {
@@ -892,38 +846,6 @@ const FieldBuilder = ({
         "multiinput",
       ],
     },
-    // {
-    //   label: "User Group Access",
-    //   type: "checkbox",
-    //   name: "userGroup",
-    //   options: [
-    //     { label: "Admin", value: "admin" },
-    //     { label: "User", value: "user" },
-    //   ],
-    //   apply: [
-    //     "text",
-    //     "email",
-    //     "number",
-    //     "textarea",
-    //     "file",
-    //     "select",
-    //     "radio",
-    //     "checkbox",
-    //     "password",
-    //     "date",
-    //     "selectindustry",
-    //     "selectsubindustry",
-    //     "selectcountry",
-    //     "selectcity",
-    //     "selectcurrency",
-    //     "selectlandline",
-    //     "selectstate",
-    //     "selectdepartment",
-    //     "editor",
-    //     "parentcompany",
-    //     "searchselect",
-    //   ],
-    // },
     {
       label: "User Group Access",
       type: "usergroupselect",
@@ -1242,18 +1164,6 @@ const FieldBuilder = ({
       defaultValue: formBuilderUpdateData?.buttonType || "button",
       apply: ["button", "buttonupdate"],
     },
-    // {
-    //   label: "Button Location",
-    //   type: "radio",
-    //   name: "buttonLocation",
-    //   options: [
-    //     { label: "Left", value: "left" },
-    //     { label: "Center", value: "center" },
-    //     { label: "Right", value: "right" },
-    //   ],
-    //   defaultValue: formBuilderUpdateData?.buttonLocation || "left",
-    //   apply: ["button", "buttonupdate"],
-    // },
     {
       label: "Button Class",
       type: "text",
@@ -1497,13 +1407,6 @@ const FieldBuilder = ({
     formBuilderUpdateData?.visible ? formBuilderUpdateData.visible : []
   );
 
-  // // Condition validation state
-  // const [validationConditionList, setValidationConditionList] = useState(
-  //   formBuilderUpdateData?.conditionValidation
-  //     ? formBuilderUpdateData.conditionValidation
-  //     : []
-  // );
-
   // Key copy condition state
   const [copyConditionList, setCopyConditionList] = useState(
     formBuilderUpdateData?.copyFields
@@ -1689,22 +1592,6 @@ const FieldBuilder = ({
       formik.setFieldValue(["name"], uuidKey);
     }
   }, [formik?.values["name"]]);
-
-  // Do not delete (Need for reference)
-  // useEffect(() => {
-  //   if (formik?.values["label"] && formik?.values["name"] !== "") {
-  //     if (formik?.values["label"]) {
-  //       formik.setFieldValue(
-  //         ["name"],
-  //         `${toCamelCase(formik?.values["label"])}`
-  //       );
-  //     }
-  //   }
-
-  //   if (formik?.values["label"] === "") {
-  //     formik.setFieldValue(["name"], uuidKey);
-  //   }
-  // }, [formik?.values["label"]]);
 
   useEffect(() => {
     if (
