d02b46f5e8961f99cc46e14082adf0d9019e52dd
rku-avensys <rahul@aven-sys.com>
2024-02-20 12:15:56 +0530

    removed console.logs

--- template/src/components/TemplateBuilder/TemplateBuilder.jsx
+++ template/src/components/TemplateBuilder/TemplateBuilder.jsx
@@ -287,7 +287,6 @@ const TemplateBuilder = forwardRef(
         const inlineContent2 =
           TemplateHelper.addCssStyleForAlignAttribute(inlineContent);
 
-        console.log("Inline Content 2: ", inlineContent2);
         setTemplateContent(inlineContent2);
       } catch (err) {}
     };
