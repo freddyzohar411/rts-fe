994d539d391a52b2a8090c699c6a2b95b83af298
sabrina <142870152+sbn-avensys@users.noreply.github.com>
2024-03-01 17:09:22 +0800

    FOD Recruiter Selection UI Fix

--- job/src/components/JobListing/JobListing.jsx
+++ job/src/components/JobListing/JobListing.jsx
@@ -31,6 +31,9 @@ import {
 import { useUserAuth } from "@workspace/login";
 import { RECRUITER_GROUP } from "../../helpers/constant";
 import JobTagCanvas from "./JobTagCanvas";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
+import { truncate } from "@workspace/common/src/helpers/string_helper";
 
 const JobListing = () => {
   const { Permission, checkAllPermission, checkAnyRole, Role } = useUserAuth();
@@ -119,9 +122,13 @@ const JobListing = () => {
 
   // Fetch the job when the pageRequest changes
   useEffect(() => {
-    const request = { ...pageRequest, jobType: gridView};
+    const request = { ...pageRequest, jobType: gridView };
     if (checkAnyRole([Role.ADMIN])) {
-      dispatch(fetchJobLists(DynamicTableHelper.cleanPageRequest({...request, isGetAll: true})));
+      dispatch(
+        fetchJobLists(
+          DynamicTableHelper.cleanPageRequest({ ...request, isGetAll: true })
+        )
+      );
     } else {
       dispatch(fetchJobLists(DynamicTableHelper.cleanPageRequest(request)));
     }
@@ -307,40 +314,40 @@ const JobListing = () => {
                             </div>
                             {nestedVisible[index] && (
                               <ul className="d-flex flex-row justify-content-start gap-3 ps-0 ms-0">
-                                <div className="d-flex flex-column justify-content-center align-items-center">
-                                  <div className="styled-dropdown ball-1"></div>
-                                  <div className="styled-line"></div>
-                                  <div className="styled-dropdown ball-2"></div>
-                                </div>
                                 <div className="ps-0 ms-0 w-100">
-                                  {item.subNames.map((subName, subIndex) => {
-                                    const split = subName?.split("@");
-                                    return (
-                                      <li
-                                        key={subIndex}
-                                        className="d-flex flew-row align-items-center justify-content-between"
-                                      >
-                                        {split[1]}
-                                        <Label
-                                          check
-                                          className="d-flex flex-row align-items-center gap-2 mb-0 ms-2"
+                                  <SimpleBar
+                                    className="simplebar-hght"
+                                    autoHide={false}
+                                  >
+                                    {item.subNames.map((subName, subIndex) => {
+                                      const split = subName?.split("@");
+                                      return (
+                                        <li
+                                          key={subIndex}
+                                          className="d-flex flew-row align-items-center justify-content-between me-3"
                                         >
-                                          <Input
-                                            type="checkbox"
-                                            checked={selectedRecruiter.includes(
-                                              parseInt(split[0])
-                                            )}
-                                            onChange={(e) =>
-                                              handleFODCheck(
-                                                parseInt(split[0]),
-                                                e.target.checked
-                                              )
-                                            }
-                                          />
-                                        </Label>
-                                      </li>
-                                    );
-                                  })}
+                                          {truncate(split[1], 16)}
+                                          <Label
+                                            check
+                                            className="d-flex flex-row align-items-center gap-2 mb-0 ms-2"
+                                          >
+                                            <Input
+                                              type="checkbox"
+                                              checked={selectedRecruiter.includes(
+                                                parseInt(split[0])
+                                              )}
+                                              onChange={(e) =>
+                                                handleFODCheck(
+                                                  parseInt(split[0]),
+                                                  e.target.checked
+                                                )
+                                              }
+                                            />
+                                          </Label>
+                                        </li>
+                                      );
+                                    })}
+                                  </SimpleBar>
                                 </div>
                               </ul>
                             )}
