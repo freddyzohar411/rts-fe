79733b74237d0328312f36ad42c0cbc92d085707
rku-avensys <rahul@aven-sys.com>
2024-04-05 18:02:40 +0530

    job overview fixes

--- job/src/pages/JobManage/JobManage.jsx
+++ job/src/pages/JobManage/JobManage.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { CustomNav } from "@workspace/common";
 import { Container, Breadcrumb, BreadcrumbItem, Row, Col } from "reactstrap";
 import { StringHelper } from "@workspace/common";
 import { Link } from "react-router-dom";
@@ -8,22 +7,29 @@ import { useSelector } from "react-redux";
 import LoadingOverlay from "react-loading-overlay";
 import JobCreation from "../../components/JobCreation/JobCreation";
 import JobOverview from "../../components/JobOverview/JobOverview";
+import {
+  Card,
+  CardBody,
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+} from "reactstrap";
+import classnames from "classnames";
 
 const JobManage = () => {
   const { jobId, slug } = useParams();
   const jobTagMeta = useSelector((state) => state.JobStageReducer.jobTagMeta);
+  const navState = location.state;
 
-  const navContents = [
-    { link: "Snapshot", url: `/jobs/${jobId}/snapshot`, slug: "snapshot" },
-    { link: "Overview", url: `/jobs/${jobId}/overview`, slug: "overview" },
-    {
-      link: "Performance",
-      url: `/jobs/${jobId}/performance`,
-      slug: "performance",
-    },
-    { link: "Email", url: `/jobs/${jobId}/email`, slug: "email" },
-    { link: "Task", url: `/jobs/${jobId}/task`, slug: "task" },
-  ];
+  // Tabs
+  const [ugTab, setUgTab] = useState(navState?.ugTab || "1");
+  const toggleUg = (tab) => {
+    if (ugTab !== tab) {
+      setUgTab(tab);
+    }
+  };
 
   return (
     <LoadingOverlay
@@ -45,19 +51,119 @@ const JobManage = () => {
               </Breadcrumb>
             </Col>
           </Row>
-
-          <CustomNav navContents={navContents} slug={slug}>
-            {slug == "snapshot" && (
-              <div className="mt-3">
-                <JobCreation />
-              </div>
-            )}
-            {slug == "overview" && (
-              <div>
-                <JobOverview />
-              </div>
-            )}
-          </CustomNav>
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames(
+                  {
+                    active: ugTab === "1",
+                  },
+                  "cursor-pointer"
+                )}
+                onClick={() => {
+                  toggleUg("1");
+                }}
+              >
+                <span>Overview</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames(
+                  {
+                    active: ugTab === "2",
+                  },
+                  "cursor-pointer"
+                )}
+                onClick={() => {
+                  toggleUg("2");
+                }}
+              >
+                <span>Snapshot</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames(
+                  {
+                    active: ugTab === "3",
+                  },
+                  "cursor-pointer"
+                )}
+                onClick={() => {
+                  toggleUg("0");
+                }}
+              >
+                <span>Email</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames(
+                  {
+                    active: ugTab === "4",
+                  },
+                  "cursor-pointer"
+                )}
+                onClick={() => {
+                  toggleUg("0");
+                }}
+              >
+                <span>Performace</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames(
+                  {
+                    active: ugTab === "5",
+                  },
+                  "cursor-pointer"
+                )}
+                onClick={() => {
+                  toggleUg("0");
+                }}
+              >
+                <span>Task</span>
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={ugTab}>
+            {/*OVERVIEW*/}
+            <TabPane tabId="1" id="manageOverview">
+              <Card>
+                <CardBody>
+                  <JobOverview />
+                </CardBody>
+              </Card>
+            </TabPane>
+            {/*SNAPSHOT*/}
+            <TabPane tabId="2" id="manageSnapshot">
+              <Card>
+                <CardBody>
+                  <JobCreation />
+                </CardBody>
+              </Card>
+            </TabPane>
+            {/*EMAIL*/}
+            <TabPane tabId="3" id="manageSnapshot">
+              <Card>
+                <CardBody>TBA</CardBody>
+              </Card>
+            </TabPane>
+            {/*PERFORMANCE*/}
+            <TabPane tabId="4" id="manageSnapshot">
+              <Card>
+                <CardBody>TBA</CardBody>
+              </Card>
+            </TabPane>
+            {/*TASK*/}
+            <TabPane tabId="5" id="manageSnapshot">
+              <Card>
+                <CardBody>TBA</CardBody>
+              </Card>
+            </TabPane>
+          </TabContent>
         </Container>
       </div>
     </LoadingOverlay>
