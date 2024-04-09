326eb5722eb5c3be024881ebb9aea73a28f8bfb9
hxg-avensys <H.Xiang@aven-sys.com>
2024-04-09 09:38:52 +0800

    Updated with enhance porposed conditional validation

--- job/src/components/JobListing/JobListing.jsx
+++ job/src/components/JobListing/JobListing.jsx
@@ -90,6 +90,19 @@ const JobListing = () => {
         </Badge>
       ),
     },
+    {
+      names: ["jobSubmissionData.jobTitle"],
+      render: (data) => {
+        return (
+          <Link
+            to={`/jobs/${data.id}/snapshot`}
+            className="text-custom-primary text-decoration-underline"
+          >
+            <span>{data?.jobSubmissionData?.jobTitle}</span>
+          </Link>
+        );
+      },
+    },
   ];
 
   // Clone Job Function
@@ -311,22 +324,22 @@ const JobListing = () => {
       //     </div>
       //   ),
       // },
-      {
-        header: "Job Title",
-        name: "jobTitle",
-        sort: true,
-        sortValue: "job_submission_data.jobTitle",
-        render: (data) => {
-          return (
-            <Link
-              to={`/jobs/${data.id}/snapshot`}
-              className="text-custom-primary text-decoration-underline"
-            >
-              <span>{data?.jobSubmissionData?.jobTitle}</span>
-            </Link>
-          );
-        },
-      },
+      // {
+      //   header: "Job Title",
+      //   name: "jobTitle",
+      //   sort: true,
+      //   sortValue: "job_submission_data.jobTitle",
+      //   render: (data) => {
+      //     return (
+      //       <Link
+      //         to={`/jobs/${data.id}/snapshot`}
+      //         className="text-custom-primary text-decoration-underline"
+      //       >
+      //         <span>{data?.jobSubmissionData?.jobTitle}</span>
+      //       </Link>
+      //     );
+      //   },
+      // },
       ...customConfig,
       {
         header: "Action",
