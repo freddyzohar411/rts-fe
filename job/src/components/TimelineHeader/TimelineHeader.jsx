99964e397ccdf1526ea3b223535e21150379b12f
sabrina <142870152+sbn-avensys@users.noreply.github.com>
2024-05-16 15:53:37 +0800

    Timeline Header Changes

--- job/src/components/TimelineHeader/TimelineHeader.jsx
+++ job/src/components/TimelineHeader/TimelineHeader.jsx
@@ -15,23 +15,44 @@ function TimelineHeader({ data }) {
     } else {
       setItemsPerPage(6);
     }
-  }, [itemsPerPage]);
+  }, [isMobile, isTablet]);
 
   const jobTimelineCount = useSelector(
     (state) => state.JobStageReducer.jobTimelineCount
   );
 
   const [counts, setCounts] = useState({});
+  // default to 1 to avoid division by zero
+  const [maxCount, setMaxCount] = useState(1); 
 
   useEffect(() => {
     let data = {};
     if (jobTimelineCount?.length > 0) {
-      jobTimelineCount?.map((item) => {
-        data[item?.name] = item?.count;
+      jobTimelineCount.forEach((item) => {
+        data[item.name] = item.count;
       });
       setCounts(data);
+      const max = Math.max(...jobTimelineCount.map((item) => item.count));
+      setMaxCount(max);
     }
   }, [jobTimelineCount]);
+
+  const filteredData = data
+    .filter((item) => item !== "Profile Feedback Pending")
+    .map((item) => {
+      switch (item) {
+        case "Interview Scheduled":
+          return "Scheduled";
+        case "Interview Happened":
+          return "Completed";
+        case "Interview Cancelled/Backout":
+          return "Cancelled/Backout";
+        case "Interview Pending Feedback":
+          return "Pending Feedback";
+        default:
+          return item;
+      }
+    });
 
   return (
     <div
@@ -41,21 +62,37 @@ function TimelineHeader({ data }) {
           : "flex-nowrap align-items-start"
       }`}
     >
-      {data.map((item, index) => (
+      {filteredData.map((item, index) => (
         <div
           key={index}
-          className={`d-flex flex-column align-items-center justify-content-top gap-3 p-1 ${
-            isMobile ? "" : "h-100"
-          }`}
+          className="d-flex flex-column align-items-center justify-content-end"
+          style={{ height: "120px" }}
         >
           <div
-            className="rounded rounded-circle bg-custom-primary text-white d-flex justify-content-center align-items-center"
-            style={{ width: "28px", height: "28px" }}
-          >
-            {counts?.[item] ?? 0}
-          </div>
-          <div className="text-center" style={{ fontSize: "12px" }}>
-            {item}
+            className="d-flex justify-content-center align-items-end mb-1"
+            style={{
+              width: "130px",
+              height:
+                counts[item] === 0
+                  ? "7.5%"
+                  : `${(counts[item] / maxCount) * 60}%`,
+              transition: "height 0.3s ease",
+              background:
+                "linear-gradient(to bottom, rgba(255, 255, 255, 0.5), #A5BEE3)",
+              color: "white",
+              alignSelf: "flex-end",
+            }}
+          ></div>
+          <div className="text-center">
+            <div className="form-text" style={{ alignSelf: "flex-end" }}>
+              {item}
+            </div>
+            <div
+              className="fw-semibold text-custom-primary"
+              style={{ alignSelf: "flex-end" }}
+            >
+              <span>{counts?.[item] ?? 0}</span>
+            </div>
           </div>
         </div>
       ))}
